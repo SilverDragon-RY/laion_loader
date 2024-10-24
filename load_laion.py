06b2548f019ad184122cad964814b7bc14506bf2
SilverDragon-RY <60697193+SilverDragon-RY@users.noreply.github.com>
2024-10-25 06:08:05 +0800

    Add files via upload

---
+++ load_laion.py
@@ -0,0 +1,99 @@
+from PIL import Image
+import requests
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+
+from datasets import load_dataset
+
+from concurrent.futures import ThreadPoolExecutor,as_completed
+
+import random
+
+import os
+
+def build_dataset(thread_cum, urls, correct_sizes, texts, save_path, timeout):
+    desc_text = "Thread:" + str(thread_cum)
+    total = len(urls)
+    success = 0
+
+    for i in tqdm(range(0, total), desc=desc_text):
+        url = urls[i]
+        correct_w, correct_h = correct_sizes[i]
+        name = str( thread_cum*total + i )
+        text = texts[i]
+
+        try:
+            # Get Web Image+size
+            IMG = Image.open(urlopen(url,timeout=timeout))
+            res_w, res_h = IMG.size
+            # Filter Blocked Images
+            if res_w == correct_w and res_h == correct_h:
+                # save image
+                pth = save_path+name+"/"
+                os.makedirs(pth)
+                IMG.save(pth+"image.png")
+                # save prompt
+                with open (pth+"text.txt", "w") as f:
+                    f.write(text)
+                success += 1
+        except:
+            pass
+    return success
+
+class LaionBuilder():
+    def __init__(self, token="", dataset_name="laion/laion2B-en-aesthetic"):
+        # Load Meta
+        print("loading meta")
+        self.ds = load_dataset(dataset_name, token=token, split="train", streaming=True)
+        self.ds = self.ds.shuffle(seed=random.randint(0, 1000))
+        self.default_path = "./" + dataset_name + "/"
+        print("meta loaded")
+
+    def load(self, num_data, save_path=".", num_workers=1, timeout=10):
+        if save_path == ".":
+            save_path = self.default_path
+        # Check path
+        if not os.path.exists(save_path):
+            # Create the directory
+            os.makedirs(save_path)
+        # Init
+        thread_cum = 0
+        # Download
+        while num_data != 0:
+            if num_data > 100:
+                # parallel
+                num_per_thread = int(num_data / num_workers)
+            else:
+                # single
+                num_per_thread = num_data
+                num_workers = 1
+            # start
+            threads = []
+            with ThreadPoolExecutor(max_workers=num_workers) as pool:
+                for i in range(0, num_workers):
+                    this_thread_id = thread_cum
+                    # Get shard
+                    if i==num_workers-1:
+                        this_ds = list(self.ds.take(num_data - num_per_thread * (num_workers-1) ))
+                    else:
+                        this_ds = list(self.ds.take(num_per_thread))
+                    # Calc
+                    urls = [ent["URL"] for ent in this_ds]
+                    correct_sizes = [ [ent["WIDTH"], ent["HEIGHT"]] for ent in this_ds]
+                    texts = [ent["TEXT"] for ent in this_ds]
+                    # Start Thread
+                    threads.append(pool.submit(build_dataset, this_thread_id, urls, correct_sizes, texts, save_path, timeout))
+                    # Skip Used
+                    self.ds = self.ds.skip(num_per_thread)
+                    thread_cum += 1
+                # Summarize
+                for thread in as_completed(threads):
+                    num_data - thread.result()
+                    pool.shutdown(wait=True)
+            # batch summarize
+            print("cycle complete. remaining tasks: ", num_data)
+            
